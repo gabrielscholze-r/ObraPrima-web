099dd6aa8bd6b1a90fd2e08cd783e38645c7d614
gabrielscholze-r <oliverfantasma24@gmail.com>
2022-06-26 01:03:30 -0300

    order controller

--- backend/controllers/OrderController.js
+++ backend/controllers/OrderController.js
@@ -9,9 +9,50 @@ module.exports = {
         order.rating = rating
         order.rDescription = rDescription
         await order.save()
-        return re.json(order)
+        return res.json(order)
     },
     async updateStatus(req,res){
+        const {id} = req.body
+        const order = await Order.findOne({_id:id}).catch(error => {console.log(error)})
+        order.done = true
+        await order.save()
+        return res.json(order)
+    },
+    async create(req, res){
+        const {name,professionalID,costumerID,description,price,done,rating, rDescription} = req.body
+        if(!name || !professionalID || !costumerID || !description || !price || !done || !rating || !rDescription){
+            return res.status(400).json({ error: "missing information!" });
+        }
+        const newOrder = Order.create({
+            name,
+            professionalID,
+            costumerID,
+            description,
+            price,
+            done,
+            rating,
+            rDescription
+        })
+        return res.json(newOrder)
 
+        /*
+        name: String,
+    professionalID: String,
+    costumerID: String,
+    description:String,
+    price: Number,
+    done: Boolean,
+    rating, Number,
+    rDescription: String*/ 
+    },
+    async findByCostumer(req,res){
+        const {costumerID} = req.body
+        const OrderList = await Order.find({costumerID: costumerID})
+        return res.json(OrderList)
+    },
+    async findByProfessional(req,res){
+        const {professionalID} = req.body
+        const OrderList = await Order.find({professionalID: professionalID})
+        return res.json(OrderList)
     }
 }
--- backend/routes.js
+++ backend/routes.js
@@ -18,4 +18,12 @@ routes.delete('/professioal/:id', ProfessionalController.delete)
 routes.get('/professioal/find/:email',ProfessionalController.findByEmail)
 routes.get('/professioal/auth', ProfessionalController.login)
 
+const OrderController = require('./controllers/OrderController')
+
+routes.get('/order/rating', OrderController.updateRating)
+routes.get('/order/done', OrderController.updateStatus)
+routes.post('/order', OrderController.create)
+routes.get('/order/professional', OrderController.findByProfessional)
+routes.get('/order/costumer', OrderController.findByCostumer)
+
 module.exports = routes
