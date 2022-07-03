67269dab7a6dbf5028341ab7b43bf1ba03fdbbd0
gabrielscholze-r <oliverfantasma24@gmail.com>
2022-07-03 01:25:44 -0300

    login alert

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,16 +1,28 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import './index.css'
 import obraPrima from '../../assets/obra_prima.png';
 import AuthProvider from '../../config/AuthContext';
+import withReactContent from 'sweetalert2-react-content';
 import { useNavigate } from 'react-router-dom';
+import Swal from 'sweetalert2';
 
 
 function Login() {
+  const swal = withReactContent(Swal)
+  const [email, setEmail] = useState("")
+  const [senha, setSenha] = useState("")
   const [id, setID] = useContext(AuthProvider)
   let history = useNavigate()
   if (id != "" && id != undefined) {
     history('/Home')
   }
+  function login() {
+    if (email=="" || senha == ""){
+      swal.fire({icon: 'error',title: 'Ops...',text: 'Forneça email e senha!!'})
+    }
+  }
+
+
   return (
     <div className="Login">
       <header className="login-header">
@@ -19,9 +31,9 @@ function Login() {
       </header>
       <div className="login-container p-8 rounded-xl mx-auto">
         <h1 className="login-title">ENTRAR</h1>
-        <input type="text" className="login-username-input rounded-xl px-2 py-1 my-5 mx-auto mt-10" placeholder='&#xf0e0; Email' />
-        <input type="password" className="login-username-input rounded-xl px-2 py-1 my-5 mx-auto" placeholder='&#61475; Senha' />
-        <button className="log-in-button mx-auto rounded-xl py-2 mt-10">ENTRAR</button>
+        <input type="text" className="login-username-input rounded-xl px-2 py-1 my-5 mx-auto mt-10" placeholder='&#xf0e0; Email' onChange={e => { setEmail(e.target.value) }} value={email} />
+        <input type="password" className="login-username-input rounded-xl px-2 py-1 my-5 mx-auto" placeholder='&#61475; Senha' onChange={e => { setSenha(e.target.value) }} value={senha} />
+        <button className="log-in-button mx-auto rounded-xl py-2 mt-10" onClick={() => { login() }}>ENTRAR</button>
         <p className="my-4 go_register_text">
           NÃO POSSUI CONTA? <button className="go-register-button" onClick={() => { history('/Register') }}>REGISTRE AQUI!</button>
         </p>
